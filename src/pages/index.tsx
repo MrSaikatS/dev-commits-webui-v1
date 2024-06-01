04268f0f56b9bc4d45627ad4585910bbda3a4555
Saikat Sardar <saikatsardar5@gmail.com>
2024-06-01 13:22:36 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,26 @@
-import { Tabs, Tab } from "@nextui-org/tabs";
-import { Card, CardBody } from "@nextui-org/card";
 import LoginForm from "@/components/LoginForm";
 import RegisterUserForm from "@/components/RegisterUserForm";
+import { Tab, Tabs } from "@nextui-org/tabs";
 
 const index = () => {
   return (
     <>
-      <div className="flex justify-center h-dvh">
-        <div className="min-w-[300px] mt-[17%]">
-          <Tabs size="lg" color="secondary" variant="solid" fullWidth>
-            <Tab key="login" title="Login">
-              <Card>
-                <CardBody>
-                  <LoginForm />
-                </CardBody>
-              </Card>
+      <div className="flex h-dvh justify-center">
+        <div className="mt-[17%] min-w-[300px]">
+          <Tabs
+            size="lg"
+            color="secondary"
+            variant="solid"
+            fullWidth>
+            <Tab
+              key="login"
+              title="Login">
+              <LoginForm />
             </Tab>
-            <Tab key="register" title="Register">
-              <Card>
-                <CardBody>
-                  <RegisterUserForm />
-                </CardBody>
-              </Card>
+            <Tab
+              key="register"
+              title="Register">
+              <RegisterUserForm />
             </Tab>
           </Tabs>
         </div>
