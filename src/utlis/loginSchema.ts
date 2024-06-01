6d302d5e10d5b54225f19545347ecfc78f4fd370
Saikat Sardar <saikatsardar5@gmail.com>
2024-06-01 13:27:02 +0530

    Delete loginSchema.ts

--- src/utlis/loginSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  email: z.string().email({ message: "Plz Enter A Valid Email Address" }),
-  password: z.string().min(10).max(20),
-});
-
-export type loginSchemaType = z.infer<typeof loginSchema>;
