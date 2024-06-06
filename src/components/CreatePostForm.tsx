06f76a9092cf682d400e94e475b30c9486bf95aa
Priyangsu Banik <136341993+NxtNinja@users.noreply.github.com>
2024-06-06 19:11:55 +0530

    class file-upload

--- src/components/CreatePostForm.tsx
+++ src/components/CreatePostForm.tsx
@@ -6,81 +6,95 @@ import { Card, CardBody } from "@nextui-org/card";
 import { Input } from "@nextui-org/input";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import { Image } from "lucide-react";
+import { useFilePicker } from "use-file-picker";
 
 const CreatePostForm = () => {
-  const { register, handleSubmit, setValue } = useForm<postSchemaType>({
+  const [selectedImage, setSelectedImage] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<postSchemaType>({
     resolver: zodResolver(postSchema),
   });
 
-  const [imagePrev, setImagePrev] = useState<string | null>(null);
+  const { openFilePicker, filesContent, plainFiles, clear } = useFilePicker({
+    multiple: false,
+    accept: "image/*",
+    readAs: "DataURL",
+    onFilesSuccessfullySelected: () => {
+      setSelectedImage(true);
+    },
+    onClear: () => {
+      setSelectedImage(false);
+    },
+  });
+
+  const clearImg = () => {
+    clear();
+    reset();
+  };
 
   const postFn = (postData: postSchemaType) => {
     console.log(postData);
+    console.log(plainFiles);
   };
-
-  const selectImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImagePrev(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-      setValue("postImage", file);
-    }
-  };
-
   return (
     <>
       <div className="flex w-full items-center justify-center sm:h-2/3 sm:w-1/2">
         <Card className="h-full w-full">
           <CardBody className="h-full">
-            <form
-              action=""
-              className="flex flex-col space-y-5"
-              onSubmit={handleSubmit(postFn)}>
-              <div className="flex h-[290px] flex-col items-center">
-                {imagePrev ? (
-                  <div className="h-[290px] p-1">
-                    <img
-                      src={imagePrev}
-                      alt="Selected"
-                      className="h-full w-full object-cover"
-                    />
-                  </div>
-                ) : (
-                  <label className="flex h-full w-full cursor-pointer items-center justify-center rounded border border-dashed border-gray-400 p-4 text-center">
-                    <div className="flex h-full w-full items-center justify-center">
-                      <div className="flex flex-col items-center justify-evenly gap-2">
-                        <Image size={90} />
-                        <p className="font-bold">Click here to browse image</p>
-                      </div>
-                      <input
-                        {...register("postImage")}
-                        type="file"
-                        className="hidden"
-                        accept="image/*"
-                        onChange={selectImage}
-                      />
-                    </div>
-                  </label>
-                )}
-              </div>
-              <Input
-                {...register("caption")}
-                placeholder="Write something about your post"
-                radius="sm"
-                color="primary"
-                variant="underlined"
-              />
-              <Button
-                radius="sm"
-                type="submit"
-                color="primary">
-                Create Post
-              </Button>
-            </form>
+            <div className="flex h-[290px] flex-col items-center">
+              {filesContent.map((file, index) => (
+                <img
+                  src={file.content}
+                  key={index}
+                  alt={file.name}
+                  width={290}
+                  className="aspect-square"
+                />
+              ))}
+
+              {!selectedImage && (
+                <Button
+                  color="primary"
+                  onPress={openFilePicker}>
+                  Select File
+                </Button>
+              )}
+            </div>
+            {selectedImage && (
+              <form
+                action=""
+                className="flex flex-col space-y-5"
+                onSubmit={handleSubmit(postFn)}>
+                <Input
+                  {...register("caption")}
+                  placeholder="Write something about your post"
+                  radius="sm"
+                  color="primary"
+                  variant="underlined"
+                  errorMessage={errors.caption?.message}
+                  isInvalid={!!errors.caption?.message}
+                />
+                <Button
+                  radius="sm"
+                  type="submit"
+                  color="primary"
+                  variant="shadow">
+                  Create Post
+                </Button>
+                <Button
+                  onPress={clearImg}
+                  radius="sm"
+                  type="submit"
+                  color="danger"
+                  variant="shadow">
+                  Cancel
+                </Button>
+              </form>
+            )}
           </CardBody>
         </Card>
       </div>
--- src/utils/zodSchemas.ts
+++ src/utils/zodSchemas.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 export const userLoginSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(6, { message: "Password should be at least 6 characters long" }),
+  password: z
+    .string()
+    .min(6, { message: "Password should be at least 6 characters long" }),
 });
 
 export const userRegisterSchmea = z.object({
@@ -12,10 +14,15 @@ export const userRegisterSchmea = z.object({
     .max(20, { message: "Name Must be 20 or more characters long" }),
   last_name: z.string({ message: "Only string" }).min(3).max(20),
   email: z.string().email({ message: "Please enter a valid email address." }),
-  password: z.string().min(6, { message: "Password should be at least 6 characters long" }).max(20),
+  password: z
+    .string()
+    .min(6, { message: "Password should be at least 6 characters long" })
+    .max(20),
 });
 
 export const postSchema = z.object({
-  caption: z.string(),
-  postImage: z.instanceof(File),
+  caption: z
+    .string()
+    .min(5, { message: "Caption should be atleast 5 characters" }),
+  // postImage: z.instanceof(File),
 });
